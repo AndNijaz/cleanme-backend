65e61ef86498938c75d38ba54c2fca9b100e68c0
bejtoni <bejtic.alen@gmail.com>
2025-05-10 15:22:05 +0200

    ADDED: DTOs needed for JWT auth

---
+++ src/main/java/com/cleanme/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.cleanme.dto;
+
+import com.cleanme.enums.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private UserType userType;
+}
